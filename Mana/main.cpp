5ea8bac751c648c34ce054e3f9fd5960dc32af5a
filipematos95 <filipematos@Air-de-Filipe-2.home>
2015-05-19 23:12:55 +0100

    Initial Commit

---
+++ Mana/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Mana
+//
+//  Created by Filipe  Matos on 19/05/15.
+//  Copyright (c) 2015 Filipe  Matos. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
